3b546c11b6b7970452a8885bd9a09ac68de66952
YamazakiYusuke <yusuke38098@gmail.com>
2024-09-05 07:30:31 +0900

    Merge branch '40_button_component' into 41_to_do_list

--- app/components/UI/NavigationButton.tsx
+++ app/components/UI/NavigationButton.tsx
@@ -1,7 +1,7 @@
 
-export default function NavigationButton({ title, id, onClick }: { title: string, id: string, onClick?: () => void }) {
+export default function NavigationButton({ title, onClick }: { title: string, onClick?: () => void }) {
   return(
-    <div className="bg-transparent rounded block flex items-center" id={id}>
+    <div className="bg-transparent rounded block flex items-center">
       <button 
         className="bg-transparent hover:bg-gray-500 text-gray-500 hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded"
         onClick={onClick}
